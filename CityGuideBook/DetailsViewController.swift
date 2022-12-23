f2b2dd2743a140877c65d868237b813bab8068ca
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-24 00:22:02 +0100

    City Guide book pproject has been started.

---
+++ CityGuideBook/DetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailsViewController.swift
+//  CityGuideBook
+//
+//  Created by Bedirhan Köse on 24.12.22.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var CityNameLabel: UILabel!
+    @IBOutlet weak var CityRegionLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
--- CityGuideBook/ViewController.swift
+++ CityGuideBook/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableVIew: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
